f23524048ecf68d09eea92f29c85f5cc66fb1f60
Jiahao Liu <jiahao.liu@glovoApp.com>
2025-08-24 16:31:00 +0200

    Updated the activity to enqueue compression work on new intent

--- app/src/main/java/com/jiahaoliuliu/androidworkmanager/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/androidworkmanager/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.jiahaoliuliu.androidworkmanager
 
+import android.app.ComponentCaller
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,12 +15,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.jiahaoliuliu.androidworkmanager.ui.theme.AndroidWorkManagerTheme
+import com.jiahaoliuliu.androidworkmanager.worker.PhotoCompressionWorker
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var workManager: WorkManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        workManager = WorkManager.getInstance(applicationContext)
         setContent {
             AndroidWorkManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -27,6 +41,29 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent.getParcelableExtra(Intent.EXTRA_STREAM)
+        } ?: return
+
+        val request = OneTimeWorkRequestBuilder<PhotoCompressionWorker>()
+            .setInputData(
+                workDataOf(
+                    PhotoCompressionWorker.KEY_CONTENT_URI to uri.toString(),
+                    PhotoCompressionWorker.KEY_COMPRESSION_THRESHOLD to 1024 * 20L
+                )
+            )
+            .setConstraints(Constraints(
+                requiresStorageNotLow = true,
+            ))
+            .build()
+
+        workManager.enqueue(request)
     }
 }
 
